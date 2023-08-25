d4ab697d50f85f54a6e904f007fd081544b738bc
pzstrk <57590316+pzstrk@users.noreply.github.com>
2023-08-26 02:49:02 +0900

    fix: uploading process (#9)

--- lib/config.py
+++ lib/config.py
@@ -1,6 +1,6 @@
 import json
 import os
 
-config_dir = os.getcwd()
+config_dir = os.path.dirname(os.path.abspath(__file__))
 config_file = open(os.path.join(config_dir, 'config.json'), 'r')
 config = json.load(config_file)

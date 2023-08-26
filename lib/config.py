ec2ae863ca2478722946c55a24bbfc92d0c3d4bf
pzstrk <57590316+pzstrk@users.noreply.github.com>
2023-08-26 20:32:10 +0900

    chore: fix relative path (#10)

--- lib/config.py
+++ lib/config.py
@@ -2,5 +2,5 @@ import json
 import os
 
 config_dir = os.path.dirname(os.path.abspath(__file__))
-config_file = open(os.path.join(config_dir, 'config.json'), 'r')
+config_file = open(os.path.join(config_dir, '../config.json'), 'r')
 config = json.load(config_file)
